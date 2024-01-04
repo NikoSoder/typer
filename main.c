2c7408df83f9b8f03bd5b83e43a7545d358f90c6
Niko <soder.n@gmail.com>
2024-01-04 16:59:53 +0200

    Read user input

--- main.c
+++ main.c
@@ -4,4 +4,39 @@
 int main(void)
 {
     // code
+    while ((user_char = getch()) != '\n')
+    {
+        //  clear();
+        // y--; UP
+        // y++; DOWN
+        // x--; LEFT
+        // x++; RIGHT
+
+        if (user_char == short_text[x])
+        {
+            attron(COLOR_PAIR(GREEN_TEXT)); // Turn on color
+            printw("%c", user_char);
+            attroff(COLOR_PAIR(GREEN_TEXT)); // Turn off color
+            x++;
+        }
+        else if (user_char != short_text[x])
+        {
+            attron(COLOR_PAIR(RED_TEXT));
+            printw("%c", user_char);
+            attroff(COLOR_PAIR(RED_TEXT));
+            x++;
+        }
+
+        // break if end of text
+        if (x == short_text_length)
+        {
+            break;
+        }
+
+        // Move the cursor to the new position
+        move(y, x);
+
+        // Refresh the screen
+        // refresh();
+    }
 }
